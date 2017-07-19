bc7bca2d01b2b67b301c8d65ef39435812b73e96
phoenixIS <savchira9@gmail.com>
2017-07-19 09:05:28 +0300

    Add interface methods

--- CatalogService/Storage/IRepositoryFactory.cs
+++ CatalogService/Storage/IRepositoryFactory.cs
@@ -1,9 +1,14 @@
-﻿using CatalogService.StorageItems;
+﻿using System.Collections.Generic;
+using CatalogService.StorageItems;
 
 namespace CatalogService.Storage
 {
     public interface IRepositoryFactory<T> where T : StorageItem
     {
         AbstractRepository<T> Create();
+
+        List<T> GetItems();
+
+        void SaveItems(List<T> items);
     }
 }
